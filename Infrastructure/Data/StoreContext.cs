06b3b3184724bb4dbe0635ac544ee8d9897e9511
samir <samir@SAMIRMAGDY>
2023-11-20 16:22:50 +0200

    Add project files.

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    internal class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Products> Products { get; set; } = default!;
+    }
+}
